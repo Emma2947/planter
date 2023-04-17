4c4a2d71fdfc50a55475ebbfe76cdf384293c636
eaakathj <eaakathj@students.eaaa.dk>
2023-04-17 22:48:58 +0200

    Kathrine

---
+++ js/animation.js
@@ -0,0 +1,8 @@
+/*Virker ikke efter hensigten*/ 
+var video = document.createElement("animations-video");
+video.setAttribute("height", "360");
+video.setAttribute("width", "640");
+video.setAttribute("id", "animations-video");
+video.setAttribute("controls", "controls");
+document.body.appendChild("animations-video");
+document.getElementById("animations-video").play();
