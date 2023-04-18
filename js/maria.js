6fd96c1c4163b2aaa260e805d92cff3752aa7e83
Emmaesther <112545493+Emmaesther@users.noreply.github.com>
2023-04-18 10:38:06 +0200

    Emma

--- js/maria.js
+++ js/maria.js
@@ -7,3 +7,24 @@ window.addEventListener('scroll', function() {
       element.classList.add('slide-in');
     }
   });
+
+  window.addEventListener('scroll', function() {
+    var element = document.querySelector('.citat-maria');
+    var position = element.getBoundingClientRect();
+    var offset = window.innerHeight * 0.7; // adjust as needed
+  
+    if (position.top < offset) {
+      element.classList.add('slide-in');
+    }
+  });
+
+  window.addEventListener('scroll', function() {
+    var element = document.querySelector('.brødtekst-maria');
+    var position = element.getBoundingClientRect();
+    var offset = window.innerHeight * 0.7; // adjust as needed
+  
+    if (position.top < offset) {
+      element.classList.add('slide-in');
+    }
+  });
+
